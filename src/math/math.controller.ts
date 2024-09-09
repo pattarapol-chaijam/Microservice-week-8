2a4a47db6fe9577c080f461d962a0edd678839e1
Pattarapol Chaijam <64160249@go.buu.ac.th>
2024-09-09 16:53:28 +0700

    add logger

--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { MathService } from './math.service';
 
 @Controller('math')
 export class MathController {
+  private logger = new Logger('MathController');
   constructor(private readonly mathservice: MathService) {}
   @Post('sum')
   sum(@Body('numbers') numbers: number[]): number {
+    this.logger.log('sum ' + numbers);
     return this.mathservice.accumulate(numbers);
   }
 }
